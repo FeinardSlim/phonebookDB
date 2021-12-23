598eea164c7a73d2a1c05c1c5f792f75879668f6
FeinardSlim <feinard1234@gmail.com>
2021-12-23 14:49:42 +0700

    update on phonebook

--- PhoneBookModel.py
+++ PhoneBookModel.py
@@ -1,46 +1,87 @@
-import shelve
 import uuid
+import mysql.connector
+
+config = dict()
+config = {
+    'user': 'root',
+    'password': 'root',
+    'host': '34.68.0.204',
+    'database': 'phonebookdb'
+}
 
 class PhoneBook:
     def __init__(self):
+        self.conn = mysql.connector.connect(config)
+        self.cursor = self.conn.cursor()
         self.namafile = 'phonebook.db'
-        self.db = shelve.open(self.namafile,writeback=True)
+        self.cursor.execute(
+            " CREATE TABLE IF NOT EXISTS `phonebook` ("
+              "`id` varchar(50) NOT NULL,"
+              "`nama` varchar(50) NOT NULL,"
+              "`alamat` text NOT NULL,"
+              "`notelp` varchar(50) NOT NULL,"
+              "`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+              "`updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+              "PRIMARY KEY (`id`)"
+            ") ENGINE=InnoDB DEFAULT CHARSET=latin1;"
+        )
+        self.conn.commit()
+
+    def commit(self,query):
+        self.cursor.execute(query)
+        return self.conn.commit()
+
     def list(self):
         data = []
         try:
-            for i in self.db.keys():
-                data.append(dict(id=i,data=self.db[i]))
+            query = "select * from phonebook"
+            self.cursor.execute(query)
+            data = self.cursor.fetchall()
+            # for i in self.db.keys():
+            #     data.append(dict(id=i,data=self.db[i]))
             return dict(status='OK',data=data)
         except:
             return dict(status='ERR',msg='Error')
+
     def create(self,info):
         try:
             id = str(uuid.uuid1())
-            self.db[id] = info
-            return dict(status='OK',id=id)
+            # self.db[id] = info
+            query = "insert into phonebook (id,nama,alamat,notelp) values (%s,%s,%s,%s)",(id,info['nama'],info['alamat'],info['notelp'],)
+            if self.commit(query):
+                return dict(status='OK',id=id)
+            else:
+                raise ValueError('query gagal')
         except:
             return dict(status='ERR',msg='Tidak bisa Create')
+
     def delete(self,id):
         try:
-            del self.db[id]
-            return dict(status='OK',msg='{} deleted' . format(id), id=id)
+            query = "delete from phonebook where id = %s",(id)
+            if self.commit(query):
+                return dict(status='OK',msg='{} deleted' . format(id), id=id)
+            else:
+                raise ValueError('query gagal')
         except:
             return dict(status='ERR',msg='Tidak bisa Delete')
+
     def update(self,id,info):
         try:
-            self.db[id]=info
-            return dict(status='OK',msg='{} updated' . format(id), id=id)
+            query = 'update phonebook set nama = %s, alamat = %s, notelp = %s where id = %s',(info['nama'],info['alamat'],info['notelp'],id,)
+            if self.commit(query):
+                return dict(status='OK',msg='{} updated' . format(id), id=id)
+            else:
+                raise ValueError("query gagal")
         except:
             return dict(status='ERR',msg='Tidak bisa Update')
     def read(self,id):
         try:
-            return dict(status='OK',id=id,data=self.db[id])
+            query = "select * from phonebook where id = %s",(id,)
+            self.cursor.execute(query)
+            data = self.cursor.fetchall()
+            return dict(status='OK',msg=data)
         except:
             return dict(status='ERR',msg='Tidak Ketemu')
-
-
-
-
 
 if __name__=='__main__':
     pd = PhoneBook()
